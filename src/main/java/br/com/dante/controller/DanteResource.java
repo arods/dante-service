cb068681e06fee2bdf0940522987444be3bef54c
Eduardo <eduardomelle@gmail.com>
2022-06-16 19:17:29 -0300

    Correções e melhorias

--- src/main/java/br/com/dante/controller/DanteResource.java
+++ src/main/java/br/com/dante/controller/DanteResource.java
@@ -1,15 +1,21 @@
 package br.com.dante.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dante")
 public class DanteResource {
-    
-    @GetMapping("/phone")
-    public String getPhone(){
-        return "99999-9999";
-    }
+
+	@GetMapping("/phone")
+	public String getPhone(@RequestHeader Map<String, String> headers) {
+		headers.forEach((k, v) -> System.out.println("Key(Interface) = " + k + ", Value(Implementation class) = " + v));
+
+		return "99999-9999";
+	}
+
 }
